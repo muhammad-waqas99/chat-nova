cce70676329433a47822b2162165d1ad8e678088
muhammad waqas <muhammadwaqas.dev99@gmail.com>
2026-07-14 05:45:22 +0500

    Add media upload infrastructure with Multer and ImageKit

---
+++ backend/src/lib/imageKit.js
@@ -0,0 +1,28 @@
+import ImageKit , {toFile} from '@imagekit/nodejs'
+const imageKit =new ImageKit({privateKey:process.env.IMAGE_KIT_SECRET})
+
+function hasImageKitConfig(){
+    return Boolean(process.env.IMAGE_KIT_SECRET)
+}
+
+ function createFileName(originalName = "upload"){
+    const safeName =originalName.replace(/[^a-zA-Z0-9._-]/g ,"_");
+    return `chat-${Date.now()}-${safeName}`
+
+}
+
+async function uploadChatMedia(file){
+    const fileName = createFileName(file.originalname);
+
+    const result = await imageKit.files.upload({
+        file: await toFile(file.buffer, fileName , {type: file.mimetype}),
+        fileName,
+        folder: "/chat"
+    })
+
+    return result.url;
+}
+
+
+export {uploadChatMedia, hasImageKitConfig} 
+
