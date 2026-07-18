6554cc79fd0a5824f0c288e130c8d37e0757ecc2
Muhammad Waqas <muhammadwaqas.dev99@gmail.com>
2026-07-18 22:46:03 +0500

    feat: configure HeroUI with global theme and wallpaper context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-
+import { Button } from '@heroui/react';
 function App() {
   return (
     <>
 
     <h1 >My App</h1>
       <header>
+            <Button>
+      My Button
+    </Button>
         <Show when="signed-out">
           <SignInButton  type="modal"/>
           <SignUpButton  type="modal" />
