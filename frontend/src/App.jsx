e34123f5fabceebd32478c593af72c68857926aa
muhammad waqas <muhammadwaqas.dev99@gmail.com>
2026-07-18 21:46:30 +0500

    feat: configure Tailwind CSS and add frontend assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 function App() {
   return (
     <>
+
+    <h1 >My App</h1>
       <header>
         <Show when="signed-out">
           <SignInButton  type="modal"/>
