13f6299129761c8d9827ee10eb414fcd831d46ee
muhammad waqas <muhammadwaqas.dev99@gmail.com>
2026-07-21 22:29:27 +0500

    Add auth state check and loading handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
-
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import { Button } from '@heroui/react';
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import AuthPage from "./pages/AuthPage";
 import { useAuth } from "@clerk/react";
-import ChatPage from './pages/ChatPage';
-import { Navigate, Route, Routes } from 'react-router';
-import AuthPage from './pages/AuthPage';
-import { ThemeProvider } from './context/ThemeContext';
-import { WallpaperProvider } from './context/WallpaperContext';
-import PageLoader from './components/PageLoader';
+import PageLoader from "./components/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import { Toaster } from "react-hot-toast";
+
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
 
-    const { isSignedIn, isLoaded } = useAuth();
-    if (!isLoaded) return <PageLoader />;
+
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
   return (
-    <>
-    
-
-     <ThemeProvider>
+    <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
@@ -25,11 +37,10 @@ function App() {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
